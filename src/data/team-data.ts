c6ac8fcb1b8fe7307cadb655abe7477e6eaead25
shijojoj <133209223+shijojoj@users.noreply.github.com>
2025-03-04 19:28:34 +0530

    first commit

---
+++ src/data/team-data.ts
@@ -0,0 +1,54 @@
+import { ITeamDT } from "@/types/team-d-t";
+
+const team_data:ITeamDT[] = [
+  {
+    id:1,
+    image:"/assets/img/home-01/team/team-1-1.jpg",
+    designation:"Art direction",
+    name:"JANE MILLS",
+  },
+  {
+    id:2,
+    image:"/assets/img/home-01/team/team-1-2.jpg",
+    designation:"Art direction",
+    name:"Arju",
+  },
+  {
+    id:3,
+    image:"/assets/img/home-01/team/team-1-3.jpg",
+    designation:"Art direction",
+    name:"Shafi latheef",
+  },
+  {
+    id:4,
+    image:"/assets/img/home-01/team/team-1-4.jpg",
+    designation:"Art direction",
+    name:"Karthika",
+  },
+  {
+    id:5,
+    image:"/assets/img/home-01/team/team-1-9.jpg",
+    designation:"Art direction",
+    name:"Krishnadas",
+  },
+  {
+    id:6,
+    image:"/assets/img/home-01/team/team-1-6.jpg",
+    designation:"Art direction",
+    name:"Shijo",
+  },
+  {
+    id:7,
+    image:"/assets/img/home-01/team/team-1-7.jpg",
+    designation:"Art direction",
+    name:"Bandra",
+  },
+  {
+    id:8,
+    image:"/assets/img/home-01/team/team-1-8.jpg",
+    designation:"Art direction",
+    name:"Livin",
+  },
+];
+
+export default team_data;
---
+++ src/layouts/footers/footer-four.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import Image from 'next/image';
+import logo from '@/assets/img/logo/logo-white.png';
+import { RightArrow, SvgBgSm } from '@/components/svg';
+import Link from 'next/link';
+
+export default function FooterFour() {
+  return (
+    <footer>
+      <div className="tp-footer-3-area dark-bg pt-120">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-4 col-lg-6 col-md-6 mb-60">
+              <div className="tp-footer-3-widget-wrapper footer-col-3-1">
+                <div className="tp-footer-3-widget mb-40">
+                  <h4 className="tp-footer-3-title">Dream diary</h4>
+                  <div className="tp-footer-3-menu">
+                    <ul>
+                      <li><a href="#">Home</a></li>
+                      <li><a href="/about-us">About</a></li>
+                     
+                      <li><a href="#">Contact</a></li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="tp-footer-3-widget">
+                  <h4 className="tp-footer-3-title">Newsletter</h4>
+                  <div className="tp-footer-3-input-box d-flex align-items-center">
+                    <input type="text" placeholder="Enter Address..." />
+                    <button className="tp-footer-3-btn p-relative">
+                      <span className="icon-1">
+                        <RightArrow clr='#19191A' />
+                      </span>
+                      <span className="icon-2">
+                        <SvgBgSm/>
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-6 col-md-6 mb-60">
+              <div className="tp-footer-3-widget text-md-center footer-col-3-2">
+                <div className="tp-footer-3-logo-box">
+                  <p className="mb-100">
+                    Drop us a line sed id semper <br />
+                    risus in hend rerit.
+                  </p>
+                  <Link className="tp-footer-3-logo p-relative" href="/">
+                    <Image src={logo} alt="logo" />
+                  </Link>
+                  <p className="tp-footer-3-copyright">
+                    {new Date().getFullYear()} @dreamdiary <br /> © All rights reserved
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-6 col-md-6 mb-60">
+              <div className="tp-footer-3-widget-wrapper footer-col-3-3">
+                <div className="tp-footer-3-widget mb-30">
+                  <h4 className="tp-footer-3-title">Contact</h4>
+                  <div className="tp-footer-2-contact-item">
+                    <span>
+                      <a href="https://www.google.com/maps/@23.8223596,90.3656686,15z?entry=ttu"
+                      target="_blank">Dream diary, Velur kuttamkulam, Thrissur,680322</a>
+                      </span>
+                  </div>
+                  <div className="tp-footer-2-contact-item">
+                    <span>Phone: <a href="tel:+91 90722 84664">+91 90722 84664</a></span>
+                    <span>E mail: <a href="mailto:dreamdiary@gmail.com">dreamdiary@gmail.com</a></span>
+                  </div>
+                </div>
+                <div className="tp-footer-3-widget">
+                  <h4 className="tp-footer-3-title">Follow</h4>
+                  <div className="tp-footer-3-social">
+                    <a href="https://x.com/elonmusk"><i className="fa-brands fa-twitter"></i></a>
+                    <a href="https://www.facebook.com/profile.php?id=61556651561669"><i className="fa-brands fa-facebook-f"></i></a>
+                    <a href="https://www.instagram.com/dream_diary_weds_ads/"><i className="fa-brands fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/sundarpichai/"><i className="fa-brands fa-linkedin-in"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </footer>
+  )
+}
